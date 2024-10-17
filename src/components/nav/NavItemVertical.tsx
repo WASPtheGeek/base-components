2415bd3ed0c04666dc7825b5ad0cc81945adb890
WASPtheGeek <margarita.parhomenko1@gmail.com>
2024-10-18 00:29:43 +0300

    add react import

--- src/components/nav/NavItemVertical.tsx
+++ src/components/nav/NavItemVertical.tsx
@@ -1,5 +1,6 @@
 import NavItem from "./NavItem";
 import { INavItemProps } from "./types";
+import React from "react";
 
 export default function NavItemVertical(props: INavItemProps) {
   return <NavItem type="vertical" {...props} />;
--- src/components/nav/NavLinkVertical.tsx
+++ src/components/nav/NavLinkVertical.tsx
@@ -1,5 +1,6 @@
 import NavLink from "./NavLink";
 import { INavLinkProps } from "./types";
+import React from "react";
 
 export default function NavLinkVertical(props: INavLinkProps) {
   return <NavLink type="vertical" {...props} />;
