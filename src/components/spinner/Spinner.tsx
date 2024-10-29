a1caf12d3e7a0dbce7b47820557e41bb62663cf3
WASPtheGeek <margarita.parhomenko1@gmail.com>
2024-10-30 00:54:08 +0200

    add missing import

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,18 +1,23 @@
+import React from "react";
+
 function Spinner() {
-    return (
-        <svg className="h-8 w-8 animate-spin overflow-hidden m-auto" viewBox="0 0 100 100">
-            <circle
-                fill="none"
-                strokeWidth="10"
-                className="stroke-current"
-                strokeDasharray="400"
-                strokeDashoffset="220"
-                cx="50"
-                cy="50"
-                r="40"
-            />
-        </svg>
-    )
+  return (
+    <svg
+      className="h-8 w-8 animate-spin overflow-hidden m-auto"
+      viewBox="0 0 100 100"
+    >
+      <circle
+        fill="none"
+        strokeWidth="10"
+        className="stroke-current"
+        strokeDasharray="400"
+        strokeDashoffset="220"
+        cx="50"
+        cy="50"
+        r="40"
+      />
+    </svg>
+  );
 }
 
 export default Spinner;
